02181ed54aaf339d90ff0b276e832ac2f43f94f2
Yan Yunlong <18810208912@163.com>
2019-05-10 11:37:33 +0800

    first

---
+++ src/testDemo.java
@@ -0,0 +1,37 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "testDemo")
+public class testDemo extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 设置响应内容类型
+        response.setContentType("text/html;charset=UTF-8");
+
+        PrintWriter out = response.getWriter();
+        String title = "使用 GET 方法读取表单数据";
+        // 处理中文
+        String name =new String(request.getParameter("name").getBytes("ISO8859-1"),"UTF-8");
+        String docType = "<!DOCTYPE html> \n";
+        out.println(docType +
+                "<html>\n" +
+                "<head><title>" + title + "</title></head>\n" +
+                "<body bgcolor=\"#f0f0f0\">\n" +
+                "<h1 align=\"center\">" + title + "</h1>\n" +
+                "<ul>\n" +
+                "  <li><b>站点名</b>："
+                + name + "\n" +
+                "  <li><b>网址</b>："
+                + request.getParameter("url") + "\n" +
+                "</ul>\n" +
+                "</body></html>");
+    }
+}
